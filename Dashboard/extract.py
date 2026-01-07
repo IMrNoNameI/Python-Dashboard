b8526ffc79f309ebb7d2b17183273b4a00f83e7d
IMrNoNameI <juli1352@gmail.com>
2026-01-08 00:41:08 +0100

    build extract

--- Dashboard/extract.py
+++ Dashboard/extract.py
@@ -1,3 +1,9 @@
-a = 1
-b= 2
-print ("wert",a )
+import pandas as pd
+
+# ---------------
+# Extraction Part
+# ---------------
+def extract():
+    basket_df = pd.read_csv("data/raw/basket_details.csv")
+    customer_df = pd.read_csv("data/raw/customer_details.csv")
+    return basket_df, customer_df
