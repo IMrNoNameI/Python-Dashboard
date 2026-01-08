5835286fe0f8161f2c540267a749a9962af7d284
IMrNoNameI <juli1352@gmail.com>
2026-01-08 18:57:45 +0100

    new try

--- Dashboard/extract.py
+++ Dashboard/extract.py
@@ -1,9 +1,30 @@
 import pandas as pd
 
 # ---------------
-# Extraction Part
+# Extraktion
 # ---------------
 def extract():
     basket_df = pd.read_csv("data/raw/basket_details.csv")
     customer_df = pd.read_csv("data/raw/customer_details.csv")
     return basket_df, customer_df
+
+#----------------
+# Transformation
+#----------------
+
+def transform(customer_df, basket_df):
+    
+    # Setzen von Datentypen
+
+
+
+
+
+
+
+
+#----------------
+# Laden
+#----------------
+
+def load(return von transform):
